d6b799272198ce253d75a7b5dad82606bbc1e278
Vedant Deshmukh <113693797+VedantDeshmukh1@users.noreply.github.com>
2024-07-05 22:04:11 +0530

    Update app.py

--- Apps/AgriAssist/app.py
+++ Apps/AgriAssist/app.py
@@ -13,7 +13,7 @@ import toml
 #secrets = toml.load("secrets.toml")
 openai_api_key = st.secrets["openai"]["OPENAI_API_KEY"]
 
-st.title("AgriAssist: Your Smart Farming Companion for Indian Agriculture 🌍🌿")
+st.title("AgriAssist: Your Smart Farming Companion for Indian Agriculture")
 
 # Sidebar for navigation
 page = st.sidebar.selectbox("Choose a feature", ["Crop Recommendation", "Disease Identification", "Crop Calendar"])
